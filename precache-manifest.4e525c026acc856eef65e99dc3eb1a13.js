3d6a69a1542171ea0ee915709acc2139461df2f2
Kamil <developer.kamil90@gmail.com>
2019-02-14 17:03:16 +0100

    Updates

---
+++ precache-manifest.4e525c026acc856eef65e99dc3eb1a13.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "a90a83865cafeef2610fe63142430ad1",
+    "url": "/watch-ecommerce-react/static/media/p-7.a90a8386.png"
+  },
+  {
+    "revision": "c868f38630144e7e62d0",
+    "url": "/watch-ecommerce-react/static/css/main.b681f041.chunk.css"
+  },
+  {
+    "revision": "46dd835935c1d0b028a8",
+    "url": "/watch-ecommerce-react/static/js/runtime~main.46dd8359.js"
+  },
+  {
+    "revision": "f23bb9e5e88a599c1cfec1a1b32f1b81",
+    "url": "/watch-ecommerce-react/static/media/abt-3.f23bb9e5.jpg"
+  },
+  {
+    "revision": "604111251fe14fdbe7a0",
+    "url": "/watch-ecommerce-react/static/js/2.60411125.chunk.js"
+  },
+  {
+    "revision": "8141c52ab419618f3f6ed3071fd02781",
+    "url": "/watch-ecommerce-react/static/media/p-4.8141c52a.png"
+  },
+  {
+    "revision": "151e56c459970230807b8ee2ca247ffc",
+    "url": "/watch-ecommerce-react/static/media/p-2.151e56c4.png"
+  },
+  {
+    "revision": "1e69ed7c0438d4dea75f1706602fd11e",
+    "url": "/watch-ecommerce-react/static/media/p-6.1e69ed7c.png"
+  },
+  {
+    "revision": "c868f38630144e7e62d0",
+    "url": "/watch-ecommerce-react/static/js/main.c868f386.chunk.js"
+  },
+  {
+    "revision": "a206ff049cd0d9bc34a09d7523389360",
+    "url": "/watch-ecommerce-react/static/media/bnr-1.a206ff04.jpg"
+  },
+  {
+    "revision": "e962f37a298057eeadecdf3b242d5ec7",
+    "url": "/watch-ecommerce-react/static/media/bnr-2.e962f37a.jpg"
+  },
+  {
+    "revision": "b90c7eddd8e33201584d6ce5c3738da4",
+    "url": "/watch-ecommerce-react/static/media/bnr-3.b90c7edd.jpg"
+  },
+  {
+    "revision": "1b19480cfc500082ab4a909a2209e2ce",
+    "url": "/watch-ecommerce-react/static/media/abt-1.1b19480c.jpg"
+  },
+  {
+    "revision": "8181d7ba83bbea29f0bd4845b7302cf9",
+    "url": "/watch-ecommerce-react/static/media/abt-2.8181d7ba.jpg"
+  },
+  {
+    "revision": "604111251fe14fdbe7a0",
+    "url": "/watch-ecommerce-react/static/css/2.bf3fdc50.chunk.css"
+  },
+  {
+    "revision": "c9b932a13e22677c84c18ea9448ee142",
+    "url": "/watch-ecommerce-react/index.html"
+  }
+];
