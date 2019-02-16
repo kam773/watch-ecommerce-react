fb4740edc7bdd05ab0f3a3bda5f595b35640d483
Kamil <developer.kamil90@gmail.com>
2019-02-16 18:58:51 +0100

    add tests for 404 page

---
+++ src/__tests__/404.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Default from '../components/404/Default';
+import {shallow, mount} from 'enzyme';
+import renderer from 'react-test-renderer';
+
+describe("404", () => {
+    it("renders without crashing", () => {
+        const location = { pathname: '/ddd' };
+        const tree = renderer.create(<Default location={location}/>).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+    it('has h1 tag with class display-3', () => {
+        const location = { pathname: '/ddd' };
+        const wrapper = shallow(<Default location={location}/>)
+        expect(wrapper.find('h1')).toHaveLength(2);
+    });
+    it('has h2 tag', () => {
+        const location = { pathname: '/ddd' };
+        const wrapper = shallow(<Default location={location}/>)
+        expect(wrapper.find('h2')).toHaveLength(1);
+    });
+    it('has h3', () => {
+        const location = { pathname: '/ddd' };
+        const wrapper = shallow(<Default location={location}/>)
+        expect(wrapper.find('h3')).toHaveLength(1);
+    })
+
+})
--- src/__tests__/Landing.test.js
+++ src/__tests__/Landing.test.js
@@ -8,5 +8,54 @@ describe("Landing", () => {
         const tree = renderer.create(<Landing/>).toJSON();
         expect(tree).toMatchSnapshot();
     });
+    it("has figure tag", () => {
+        const wrapper = shallow(<Landing />);
+        expect(wrapper.find('figure').exists()).toEqual(true);
+    });
+    it("has figcaption tag", () => {
+        const wrapper = shallow(<Landing />);
+        expect(wrapper.find('figcaption').exists()).toEqual(true);
+    });
+    it("has h1 tag", () => {
+        const wrapper = shallow(<Landing />);
+        expect(wrapper.find('h1').exists()).toEqual(true);
+    });
+    it("has p tag", () => {
+        const wrapper = shallow(<Landing />);
+        expect(wrapper.find('p').exists()).toEqual(true);
+    });
+    it("has a title", () => {
+        const wrapper = mount(<h1>Featured Products</h1>)
+        expect(wrapper.text()).toEqual('Featured Products');
+    });
+    it("renders heading tag", () => {
+        const wrapper = shallow(<Landing/>);
+        expect(wrapper.find('h1')).toHaveLength(2);
+    });
+    it("has img tag", () => {
+        const wrapper = shallow(<Landing />);
+        expect(wrapper.find('img').exists()).toEqual(true);
+    });
+    it("has class card", () => {
+        const wrapper = mount(<Landing />);
+        expect(wrapper.find('.card').every('.card')).toEqual(true);
+    });
+    it("has class card-img-top", () => {
+        const wrapper = mount(<Landing />);
+        expect(wrapper.find('.card-img-top').every('.card-img-top')).toEqual(true);
+    });
+    it("has button to have class cart-btn", () => {
+        const wrapper = mount(<Landing />);
+        expect(wrapper.find('.cart-btn').every('.cart-btn')).toEqual(true);
+    });
+    it("has a cart icon", () => {
+        const wrapper = mount(<Landing />);
+        expect(wrapper.find('i').exists()).toEqual(true);
+    });
+    it("has button to have class cart-btn", () => {
+        const wrapper = mount(<Landing />);
+        expect(wrapper.find('.cart-btn').every('.cart-btn')).toEqual(true);
+    });
+
 
 })
